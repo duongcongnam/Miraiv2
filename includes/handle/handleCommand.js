85070e7394c92fd027aefb775df52d74a3a1ce4a
Nam <85828412+duongcongnam@users.noreply.github.com>
2022-06-04 23:12:02 +0700

    Update handleCommand.js

--- includes/handle/handleCommand.js
+++ includes/handle/handleCommand.js
@@ -2,22 +2,26 @@ module.exports = function ({ api, models, Users, Threads, Currencies }) {
    const stringSimilarity = require('string-similarity'),
         escapeRegex = (str) => str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&"),
         logger =  require("../../utils/log.js");
+    const axios = require("axios");
     const moment = require("moment-timezone");
     return async function ({ event }) {
     const dateNow = Date.now()
     const time = moment.tz("Asia/Ho_Chi_minh").format("HH:MM:ss DD/MM/YYYY");
-    const { allowInbox, PREFIX, ADMINBOT, DeveloperMode, adminOnly } = global.config;
+    const { allowInbox, PREFIX, ADMINBOT, DeveloperMode, adminOnly, privateChat } = global.config;
 
     const { userBanned, threadBanned, threadInfo, threadData, commandBanned } = global.data;
     const { commands, cooldowns } = global.client;
-    var { body, senderID, threadID, messageID } = event;
+    var { body, senderID, threadID, messageID, isGroup } = event;
 
     var senderID = String(senderID), 
         threadID = String(threadID);
     const threadSetting = threadData.get(threadID) || {}
-    
+    var prefix = threadSetting.PREFIX || PREFIX
     const prefixRegex = new RegExp(`^(<@!?${senderID}>|${escapeRegex((threadSetting.hasOwnProperty("PREFIX")) ? threadSetting.PREFIX : PREFIX )})\\s*`);
         if (!prefixRegex.test(body)) return;
+        if (!ADMINBOT.includes(senderID) && isGroup != true && privateChat == true) return api.sendMessage("[𝐌𝐨𝐝𝐞] - Chỉ Admin Bot được sử dụng lệnh trong tin nhắn riêng", threadID, messageID)
+        if (!ADMINBOT.includes(senderID) && adminOnly == true) return api.sendMessage("[𝐌𝐨𝐝𝐞] - Chỉ Admin Bot được sử dụng lệnh", threadID, messageID)
+        
         if (userBanned.has(senderID) || threadBanned.has(threadID) || allowInbox == ![] && senderID == threadID) {
             if (!ADMINBOT.includes(senderID.toString())) {
                 if (userBanned.has(senderID)) {
@@ -47,7 +51,10 @@ module.exports = function ({ api, models, Users, Threads, Currencies }) {
             for (const cmd of commandValues) allCommandName.push(cmd)
             const checker = stringSimilarity.findBestMatch(commandName, allCommandName);
             if (checker.bestMatch.rating >= 0.5) command = client.commands.get(checker.bestMatch.target);
-            else return api.sendMessage(global.getText("handleCommand", "commandNotExist", checker.bestMatch.target), threadID);
+  else {
+            let res = (await axios.get(global.config.configSetupAPI.imgLimk_nino)).data;
+ return api.sendMessage({body: global.getText("handleCommand", "commandNotExist", checker.bestMatch.target, prefix), attachment: (await axios.get(res.url, { responseType: "stream" })).data}, threadID, messageID);
+            }
         }  
         if (commandBanned.get(threadID) || commandBanned.get(senderID)) {
             if (!ADMINBOT.includes(senderID)) {
