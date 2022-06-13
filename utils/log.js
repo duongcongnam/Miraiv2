c09f5780e5f9bf04bd14a9c8b560a6443b7c4323
Nam <85828412+duongcongnam@users.noreply.github.com>
2022-06-13 21:23:37 +0700

    Update log.js

--- utils/log.js
+++ utils/log.js
@@ -1,29 +1,36 @@
 const chalk = require('chalk');
-
+function randomColor() {
+    var color = "";
+    for (var i = 0; i < 3; i++) {
+        var sub = Math.floor(Math.random() * 256).toString(16);
+        color += (sub.length == 1 ? "0" + sub : sub);
+    } 
+   return "#" + color;
+};
 module.exports = (data, option) => {
 	switch (option) {
 		case "warn":
-			console.log(chalk.yellow('[ ❕ Lỗi rồi ] » ') + data);
+				console.log(chalk.bold.hex("#ff0000").bold('» Lỗi « ') + data);
 			break;
 		case "error":
-			console.log(chalk.red('[ ❕ Lỗi rồi ] » ') + data);
+			console.log(chalk.bold.hex("#ff0000").bold('» Lỗi « ') + data);
 			break;
 		default:
-			console.log(chalk.magenta(`${option} » `) + data);
+				console.log(chalk.bold.hex(randomColor()).bold(`${option} » `) + data);
 			break;
 	}
 }
 
 module.exports.loader = (data, option) => {
 	switch (option) {
-		case "warn":
-			console.log(chalk.yellow('[D-JUKIE ] » ') + data);
+    case "warn":
+			console.log(chalk.bold.hex(randomColor()).bold(" » DC-Nam « ") + chalk.bold.hex("#8B8878").bold(data))
 			break;
 		case "error":
-			console.log(chalk.red('[D-JUKIE ] » ') + data);
+		console.log(chalk.bold.hex(randomColor()).bold(" » DC-Nam « ") + data);
 			break;
-		default:
-			console.log(chalk.green(`[D-JUKIE ] » `) + data);
+    default:
+			console.log(chalk.bold.hex(randomColor()).bold(" » DC-Nam « ") + chalk.bold.hex(randomColor()).bold(data));
 			break;
 	}
 }
