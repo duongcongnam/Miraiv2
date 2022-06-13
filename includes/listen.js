53c9644fe1113b8924e18f40b453408dfef7cdf8
Nam <85828412+duongcongnam@users.noreply.github.com>
2022-06-13 21:18:50 +0700

    Update listen.js

--- includes/listen.js
+++ includes/listen.js
@@ -4,9 +4,6 @@ module.exports = function({ api, models }) {
 				Threads = require("./controllers/threads")({ models, api }),
 				Currencies = require("./controllers/currencies")({ models });
 	const logger = require("../utils/log.js");
-	const fs = require("fs");
-	const moment = require('moment-timezone');
-	const axios = require("axios");
 
 	//////////////////////////////////////////////////////////////////////
 	//========= Push all variable from database to environment =========//
@@ -64,179 +61,34 @@ module.exports = function({ api, models }) {
 	const handleReaction = require("./handle/handleReaction")({ api, models, Users, Threads, Currencies });
 	const handleEvent = require("./handle/handleEvent")({ api, models, Users, Threads, Currencies });
 	const handleCreateDatabase = require("./handle/handleCreateDatabase")({  api, Threads, Users, Currencies, models });
-const handleUnsend = require("./handle/handleUnsend")({ api }); 
 
 	logger.loader(`====== ${Date.now() - global.client.timeStart}ms ======`);
-
-
-	//DEFINE DATLICH PATH
-	const datlichPath = __dirname + '/../modules/commands/cache/datlich.json';
-
-	//FUNCTION HOẠT ĐỘNG NHƯ CÁI TÊN CỦA NÓ, CRE: DUNGUWU
-	const monthToMSObj = {
-		1: 31 * 24 * 60 * 60 * 1000,
-		2: 28 * 24 * 60 * 60 * 1000,
-		3: 31 * 24 * 60 * 60 * 1000,
-		4: 30 * 24 * 60 * 60 * 1000,
-		5: 31 * 24 * 60 * 60 * 1000,
-		6: 30 * 24 * 60 * 60 * 1000,
-		7: 31 * 24 * 60 * 60 * 1000,
-		8: 31 * 24 * 60 * 60 * 1000,
-		9: 30 * 24 * 60 * 60 * 1000,
-		10: 31 * 24 * 60 * 60 * 1000,
-		11: 30 * 24 * 60 * 60 * 1000,
-		12: 31 * 24 * 60 * 60 * 1000
-	};
-	const checkTime = (time) => new Promise((resolve) => {
-		time.forEach((e, i) => time[i] = parseInt(String(e).trim()));
-		const getDayFromMonth = (month) => (month == 0) ? 0 : (month == 2) ? (time[2] % 4 == 0) ? 29 : 28 : ([1, 3, 5, 7, 8, 10, 12].includes(month)) ? 31 : 30;
-		if (time[1] > 12 || time[1] < 1) resolve("Tháng của bạn có vẻ không hợp lệ");
-		if (time[0] > getDayFromMonth(time[1]) || time[0] < 1) resolve("Ngày của bạn có vẻ không hợp lệ");
-		if (time[2] < 2022) resolve("Bạn sống ở kỷ nguyên nào thế?");
-		if (time[3] > 23 || time[3] < 0) resolve("Giờ của bạn có vẻ không hợp lệ");
-		if (time[4] > 59 || time[3] < 0) resolve("Phút của bạn có vẻ không hợp lệ");
-		if (time[5] > 59 || time[3] < 0) resolve("Giây của bạn có vẻ không hợp lệ");
-		yr = time[2] - 1970;
-		yearToMS = (yr) * 365 * 24 * 60 * 60 * 1000;
-		yearToMS += ((yr - 2) / 4).toFixed(0) * 24 * 60 * 60 * 1000;
-		monthToMS = 0;
-		for (let i = 1; i < time[1]; i++) monthToMS += monthToMSObj[i];
-		if (time[2] % 4 == 0) monthToMS += 24 * 60 * 60 * 1000;
-		dayToMS = time[0] * 24 * 60 * 60 * 1000;
-		hourToMS = time[3] * 60 * 60 * 1000;
-		minuteToMS = time[4] * 60 * 1000;
-		secondToMS = time[5] * 1000;
-		oneDayToMS = 24 * 60 * 60 * 1000;
-		timeMs = yearToMS + monthToMS + dayToMS + hourToMS + minuteToMS + secondToMS - oneDayToMS;
-		resolve(timeMs);
-	});
-
-
-	const tenMinutes = 10 * 60 * 1000;
-
-	logger.loader(`====== ${Date.now() - global.client.timeStart}ms ======`);
-	const checkAndExecuteEvent = async () => {
-
-		/*smol check*/
-		if (!fs.existsSync(datlichPath)) fs.writeFileSync(datlichPath, JSON.stringify({}, null, 4));
-		var data = JSON.parse(fs.readFileSync(datlichPath));
-
-		//GET CURRENT TIME
-		var timeVN = moment().tz('Asia/Ho_Chi_Minh').format('DD/MM/YYYY_HH:mm:ss');
-		timeVN = timeVN.split("_");
-		timeVN = [...timeVN[0].split("/"), ...timeVN[1].split(":")];
-
-		let temp = [];
-		let vnMS = await checkTime(timeVN);
-		const compareTime = e => new Promise(async (resolve) => {
-			let getTimeMS = await checkTime(e.split("_"));
-			if (getTimeMS < vnMS) {
-				if (vnMS - getTimeMS < tenMinutes) {
-					data[boxID][e]["TID"] = boxID;
-					temp.push(data[boxID][e]); delete data[boxID][e];
-				} else delete data[boxID][e];
-				fs.writeFileSync(datlichPath, JSON.stringify(data, null, 4));
-			};
-			resolve();
-		})
-
-		await new Promise(async (resolve) => {
-			for (boxID in data) {
-				for (e of Object.keys(data[boxID])) await compareTime(e);
-			}
-			resolve();
-		})
-		for (el of temp) {
-			try {
-				var all = (await Threads.getInfo(el["TID"])).participantIDs;
-			    all.splice(all.indexOf(api.getCurrentUserID()), 1);
-				var body = el.REASON || "MỌI NGƯỜI ƠI", mentions = [], index = 0;
-				
-			    for (let i = 0; i < all.length; i++) {
-				    if (i == body.length) body += " ‍ ";
-				    mentions.push({
-				  	  tag: body[i],
-				  	  id: all[i],
-				  	  fromIndex: i - 1
-				    });
-			    }
-			} catch (e) { return console.log(e); }
-			var out = {
-				body, mentions
-			}
-			if ("ATTACHMENT" in el) {
-				out.attachment = [];
-				for (a of el.ATTACHMENT) {
-					let getAttachment = (await axios.get(encodeURI(a.url), { responseType: "arraybuffer"})).data;
-					fs.writeFileSync(__dirname + `/../modules/commands/cache/${a.fileName}`, Buffer.from(getAttachment, 'utf-8'));
-					out.attachment.push(fs.createReadStream(__dirname + `/../modules/commands/cache/${a.fileName}`));
-				}
-			}
-			console.log(out);
-			if ("BOX" in el) await api.setTitle(el["BOX"], el["TID"]);
-			api.sendMessage(out, el["TID"], () => ("ATTACHMENT" in el) ? el.ATTACHMENT.forEach(a => fs.unlinkSync(__dirname + `/../modules/commands/cache/${a.fileName}`)) : "");
-		}
-
-	}
-	setInterval(checkAndExecuteEvent, tenMinutes/10);
-	
 
 	//////////////////////////////////////////////////
 	//========= Send event to handle need =========//
 	/////////////////////////////////////////////////
 	
-	return async (event) => {
-	  let data = JSON.parse(fs.readFileSync(__dirname + "/../modules/commands/cache/approvedThreads.json"));
-    var time = moment.tz('Asia/Ho_Chi_Minh').format('DD/MM/YYYY || HH:mm:ss');
-	  let adminBot = global.config.ADMINBOT;
-	  let pendingPath = __dirname + "/../modules/commands/cache/pendingdThreads.json";
-    
-      let threadInfo = await api.getThreadInfo(event.threadID);
-     let threadName = threadInfo.threadName ? threadInfo.threadName : await Users.getNameUser(event.threadID);
-   
-	  if (!data.includes(event.threadID) && !adminBot.includes(event.senderID) && global.config["duyet"].status == true) {
-		
-		//getPrefix
-		  const threadSetting = (await Threads.getData(String(event.threadID))).data || {};
-		  const prefix = (threadSetting.hasOwnProperty("PREFIX")) ? threadSetting.PREFIX : global.config.PREFIX;
-		  //check body
-		if (event.body && event.body == `${prefix}request`) {
-		  adminBot.forEach(e => {
-			api.sendMessage(`=====『 𝗥𝗘𝗤𝗨𝗘𝗦𝗧 』=====\n\n⏰ TIME: ${time}\n👥 Nhóm: ${threadName} \n❗ TID: ${event.threadID} \n\nĐã gửi yêu cầu được duyệt box đến bạn`, e);
-		  })
-		  return api.sendMessage(`Đã gửi yêu cầu đến ${global.config.ADMINBOT.length} admin, còn được duyệt hay không thì chịu 🐔`, event.threadID, () => {
-			let pendingData = JSON.parse(fs.readFileSync(pendingPath));
-			if (!pendingData.includes(event.threadID)) {
-			  pendingData.push(event.threadID);
-			fs.writeFileSync(pendingPath, JSON.stringify(pendingData));
-			}
-		  });
+	return (event) => {
+		switch (event.type) {
+			case "message":
+			case "message_reply":
+			case "message_unsend":
+				handleCreateDatabase({ event });
+				handleCommand({ event });
+				handleReply({ event });
+				handleCommandEvent({ event });
+
+				break;
+			case "event":
+				handleEvent({ event });
+				break;
+			case "message_reaction":
+				handleReaction({ event });
+				break;
+			default:
+				break;
 		}
-		// if (event.threadID == 7349457131746039) console.log(prefix);
-		if (event.body && event.body.startsWith(prefix)) return api.sendMessage(`Nhóm bạn chưa được admin duyệt. Để gửi yêu cầu duyệt, dùng:\n${prefix}request`, event.threadID);
-	
-  
-		
-	  };
-	  switch (event.type) {
-		case "message":
-		case "message_reply":
-		case "message_unsend":
-		  handleCreateDatabase({ event });
-		  handleCommand({ event });
-		  handleReply({ event });
-		  handleCommandEvent({ event });
-  
-		  break;
-		case "event":
-		  handleEvent({ event });
-		  break;
-		case "message_reaction":
-      handleUnsend({ event });
-		  handleReaction({ event });
-		  break;
-		default:
-		  break;
-	  }
 	};
-  };
+};
+
+//THIZ BOT WAS MADE BY ME(CATALIZCS) AND MY BROTHER SPERMLORD - DO NOT STEAL MY CODE (つ ͡ ° ͜ʖ ͡° )つ ✄ ╰⋃╯
